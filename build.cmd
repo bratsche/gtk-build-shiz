ea933381ac1de2841e0b1cfb695b5581a6dad544
Cody Russell <cody@jhu.edu>
2015-04-06 20:29:10 -0500

    Remove build.cmd

--- build.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-
-if not exist tools\FAKE\tools\Fake.exe (
-	tools\NuGet\nuget.exe install FAKE -OutputDirectory tools -ExcludeVersion
-	tools\NuGet\nuget.exe install FSharp.Data -OutputDirectory tools -ExcludeVersion
-)
