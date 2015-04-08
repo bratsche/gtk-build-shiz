f177426305ef80258bb97e8970892c3f6301e90e
Cody Russell <cody@jhu.edu>
2015-04-08 15:17:56 -0500

    Libtool style DLL name for harfbuzz

--- build.fsx
+++ build.fsx
@@ -226,7 +226,7 @@ Target "harfbuzz" <| fun _ ->
   ) |> ignore
 
   let releaseDir = Path.Combine(buildDir(), "harfbuzz-0.9.37", "win32", "libs", "Release")
-  [Path.Combine(releaseDir, "harfbuzz.dll"); Path.Combine(releaseDir, "harfbuzz.pdb")]
+  [Path.Combine(releaseDir, "libharfbuzz-0.dll"); Path.Combine(releaseDir, "harfbuzz.pdb")]
   |> CopyFiles (Path.Combine(installDir(), "bin"))
 
   CopyFiles (Path.Combine(installDir(), "include")) (Directory.GetFiles(Path.Combine(buildDir(), "harfbuzz-0.9.37", "src"), "*.h"))
