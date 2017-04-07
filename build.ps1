bb0544564b90876cd19672de84380e68068a532a
Cody Russell <cody@jhu.edu>
2017-04-07 13:14:43 -0700

    Set the nuget source when fetching FAKE and FSharp.Data
    This was working before without this. I don't know why
    it was broken today, but this seems to have fixed it.

--- build.ps1
+++ build.ps1
@@ -2,8 +2,8 @@ if (Test-Path tools\FAKE\tools\FAKE.exe) {
     "FAKE exists."
 } else {
     "FAKE doesn't exist."
-    .\tools\NuGet\nuget.exe install FAKE -OutputDirectory tools -ExcludeVersion
-    .\tools\NuGet\nuget.exe install FSharp.Data -OutputDirectory tools -ExcludeVersion
+    .\tools\NuGet\nuget.exe install FAKE -Source http://www.nuget.org/api/v2 -OutputDirectory tools -ExcludeVersion
+    .\tools\NuGet\nuget.exe install FSharp.Data -Source http://www.nuget.org/api/v2 -OutputDirectory tools -ExcludeVersion
 }
 
 $env:Path = "C:\msys32\usr\bin;$($env:Path)";
